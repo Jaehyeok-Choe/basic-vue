c19e188e2e7dc92d50dd12207af566375e876881
tbeben77 <tbeben77@gmail.com>
2021-02-12 00:25:07 +0900

    todo app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input v-model="todoText" type="text" class="w-100 p-2" placeholder="Type todo" @keyup.enter="addTodo"><hr>
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" @toggle-checkbox="toggleCheckbox" @click-delete="deleteTodo"/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Todo from '@/components/Todo.vue'
+export default {
+  components: {
+    Todo
+  },
+  data () {
+    return {
+      todoText: '',
+      todos: [
+        { id: 1, text: 'buy a car', checked: false },
+        { id: 2, text: 'play a game', checked: false }
+      ]
+    }
+  },
+  methods: {
+    addTodo (e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      })
+      this.todoText = ''
+    },
+    toggleCheckbox ({ id, checked }) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id
+      })
+      this.todos[index].checked = checked
+    },
+    deleteTodo (id) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id
+      })
+      this.todos.splice(index, 1)
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
