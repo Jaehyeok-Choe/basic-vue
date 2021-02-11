e003f5fc0aa71bd371b685a035bc779bc8ce8c52
tbeben77 <tbeben77@gmail.com>
2021-02-12 00:31:09 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <h1 class="text-center">Todo App</h1>
+    <h1 class="text-center">Todo App!</h1>
     <input v-model="todoText" type="text" class="w-100 p-2" placeholder="Type todo" @keyup.enter="addTodo"><hr>
     <Todo v-for="todo in todos" :key="todo.id" :todo="todo" @toggle-checkbox="toggleCheckbox" @click-delete="deleteTodo"/>
   </div>
